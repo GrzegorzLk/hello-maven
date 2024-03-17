afb0df02e4c73f3690a922eeed560e06f1be661a
lojek <none@none>
2024-03-17 14:29:12 +0100

    Po lab1

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,38 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
+    final int randomNumber = 4;
+    final int width = 120;
+    final int height = 20;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        App app = new App();
+        app.printBigText("Hello World!");
     }
-    
+
+    public void printBigText(String string) {
+        IRender render = new Render();
+        render.setPseudoCanvas(true);
+        IContextBuilder builder = render.newBuilder();
+        builder.width(width).height(height);
+        builder.element(new PseudoText(string, false));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
+    }
+
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return randomNumber;
     }
 }
